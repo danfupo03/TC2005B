de55e75cb47b01fc7f163b5591bf1cfe87422f6e
danfupo03 <danfupo@outlook.com>
2023-02-22 19:45:25 -0600

    Changes

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -1,17 +1,21 @@
-function checkPassword() {
-    let password = document.getElementById("password").value;
-    let cnfrmPassword = document.getElementById("cnfrm-password").value;
-    console.log(password, cnfrmPassword);
-    let message = document.getElementById("message");
+function checkPassword(){
+  let password = document.getElementById("password").value;
+  let cnfrmPassword = document.getElementById("cnfrm-password").value;
+  console.log(" Password:", password,'\n',"Confirm Password:",cnfrmPassword);
+  let message = document.getElementById("message");
 
-    if (password.lenght != 0 ) {
-      if(password == cnfrmPassword) {
-        message.textContent = "Passwords match";
-        message.style.backgroundColor = "#3ae374";
+  if(password.length != 0){
+      if(password == cnfrmPassword){
+          message.textContent = " Passwords match ";
+          message.style.color = "#1dcd59";
       }
       else{
-        message.textContent = "Passwords don't match";
-        message.style.backgroundColor = "#ff4d4d";
+          message.textContent = " Password don't match ";
+          message.style.color = "#ff4d4d";
       }
-    }
+  }
+  else {
+      message.textContent = " Please enter a password ";
+      message.style.color = "#000000";
+  }
 }
